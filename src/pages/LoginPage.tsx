99d2a512eadd58548c01d18920034e16d793707e
YamiHeike <koscielniak.z@gmail.com>
2025-02-26 23:21:37 +0100

    fixed: wrong password check

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,12 +13,12 @@ import { useState } from "react";
 import { FormAlert } from "../components/ui/FormAlert/FormAlert";
 
 export const LoginPage = () => {
+  const wrongPass = "Wrong password";
   const [form] = Form.useForm();
   const [error, setError] = useState<FormFieldError | null>(null);
   const { login } = useAuth();
   const navigate = useNavigate();
   const onFinish = async (values: AuthCredentials) => {
-    console.log("Login Submitted:", values);
     try {
       const response = await request<UserDto, AuthCredentials>(
         "POST",
@@ -29,11 +29,18 @@ export const LoginPage = () => {
       login(response.data);
       navigate("/");
     } catch (e: any) {
-      if (e.message === "Validation failed" && e.response.data.message) {
+      console.log(e);
+      if (e.response && e.response.data.message) {
         setError(e.response.data);
       } else {
-        throw e;
+        setError(e);
       }
+    }
+  };
+
+  const handleChange = () => {
+    if (error) {
+      setError(null);
     }
   };
 
@@ -43,6 +50,9 @@ export const LoginPage = () => {
   if (error) {
     emailMsg = getValidationErrorMessage(error, "email");
     passwordMsg = getValidationErrorMessage(error, "password");
+    if (error.message === wrongPass) {
+      passwordMsg = error.message;
+    }
   }
 
   return (
@@ -53,7 +63,7 @@ export const LoginPage = () => {
           Don't have an account? <NavLink to="/signup">Sign Up</NavLink>
         </>
       }
-      error={error}
+      error={error?.message === wrongPass ? null : error}
     >
       <Form form={form} onFinish={onFinish} layout="vertical">
         <Form.Item
@@ -64,17 +74,25 @@ export const LoginPage = () => {
             { type: "email", message: "Invalid email address" },
           ]}
         >
-          <Input prefix={<MailOutlined />} placeholder="Email" />
+          <Input
+            prefix={<MailOutlined />}
+            placeholder="Email"
+            onChange={handleChange}
+          />
         </Form.Item>
-        {emailMsg && <FormAlert errorMsg="Incorrect email" />}
+        {emailMsg && <FormAlert errorMsg="Invalid email format" />}
         <Form.Item
           name="password"
           label="Password"
           rules={[{ required: true, message: "Please enter your password" }]}
         >
-          <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+          <Input.Password
+            prefix={<LockOutlined />}
+            placeholder="Password"
+            onChange={handleChange}
+          />
         </Form.Item>
-        {passwordMsg && <FormAlert errorMsg="Incorrect password" />}
+        {passwordMsg && <FormAlert errorMsg="Invalid password format" />}
 
         <Form.Item>
           <Button type="primary" htmlType="submit" block>

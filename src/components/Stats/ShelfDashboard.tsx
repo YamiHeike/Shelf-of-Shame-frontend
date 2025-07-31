7eed0a0a0b8a236047bece33697437f5d6c19494
YamiHeike <koscielniak.z@gmail.com>
2025-07-31 22:55:26 +0200

    animated charts

--- src/components/Stats/ShelfDashboard.tsx
+++ src/components/Stats/ShelfDashboard.tsx
@@ -3,19 +3,30 @@ import { ErrorMessage, Loading, ScrollToggleBottom } from "../../ui";
 import { NoData } from "../../ui/NoData";
 import { StatusPie } from "./charts/StatusPie";
 import styles from "./ShelfDashBoard.module.scss";
-
+import { stagger, useAnimate } from "motion/react";
 import { GenreStatusBar } from "./charts/GenreStatusStackedBar";
 import { useShelfDataContext } from "./ShelfDataContext";
 import { GenreBar } from "./charts/GenreBar";
 import { AvgDifficultyByStatus } from "./charts/AvgDifficultyByStatus";
 import { DifficultyHistogram } from "./charts/DifficultyHistogram";
 import { SummaryPanel } from "./SummaryPanel";
+import { useEffect } from "react";
 
 export const ShelfDashboard = () => {
   const { data, loading, errored } = useShelfDataContext();
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    if (!data) return;
+    animate(
+      ".ant-col",
+      { opacity: [0, 1], y: [-20, 0] },
+      { duration: 0.4, delay: stagger(0.15) }
+    );
+  }, [data, animate]);
 
   if (loading) {
-    return <Loading />;
+    return <Loading fullscreen />;
   } else if (errored) {
     return <ErrorMessage />;
   } else if (!data) {
@@ -25,7 +36,7 @@ export const ShelfDashboard = () => {
   return (
     <>
       <SummaryPanel />
-      <div className={styles.dashboardContainer}>
+      <div className={styles.dashboardContainer} ref={scope}>
         <Row gutter={[16, 16]}>
           <Col xs={24} md={12}>
             <AvgDifficultyByStatus />
